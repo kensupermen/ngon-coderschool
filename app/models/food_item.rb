048a5b54118a75378c2ca3d29f7d0cab9dd63398
Ken <votantho93@gmail.com>
2017-02-19 00:01:30 +0700

    validate number :beer:

--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -1,7 +1,8 @@
 class FoodItem < ApplicationRecord
   belongs_to :section
-  validates :name, :price, presence: true
   has_many :orders
+  validates :name, :price, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 1 }
 
   def image_url_or_default
     image_url.presence || "http://loremflickr.com/320/240/#{name.gsub(' ', '')}"
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,4 @@
 class Order < ApplicationRecord
   belongs_to :food_item
-  validates :quantity, presence: true, numericality: true
+  validates :quantity, presence: true, numericality: {greater_than_or_equal_to: 1 }
 end
