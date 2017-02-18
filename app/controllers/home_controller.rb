b5521550e761ffd05ff3f229c321bfa1b66e8a0b
Ken <votantho93@gmail.com>
2017-02-18 23:20:20 +0700

    sorting :beer:

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,6 +9,13 @@ class HomeController < ApplicationController
     @sections = Section.all
     if params[:section_id].present?
       @current_section = Section.find(params[:section_id])
+      @food_items = @current_section.food_items
     end
+
+    if params[:sort_column]
+      @current_section = Section.find(params[:section_id])
+      @food_items = @current_section.food_items.order("#{params[:sort_column]} #{params[:sort_direction]}")
+    end
+
   end
 end
